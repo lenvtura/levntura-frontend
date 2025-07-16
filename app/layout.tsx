25da89821930958e17fd99e85a998885c0663764
hTaw99 <employee23.ticlick@gmail.com>
2025-07-16 13:57:01 +0300

    feat: integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ import { Footer } from "@/components/footer";
 import type { Metadata } from "next";
 
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Levntura",
@@ -21,9 +23,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NXW7WLQX');
+          `}
+        </Script>
+      </Head>
       <body
         className={`${Gelion.className} bg-foreground antialiased relative`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NXW7WLQX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <div style={{ minHeight: `calc(100vh - ${HEADER_HEIGHT}px)` }}>
           {children}
