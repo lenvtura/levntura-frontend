6d20e5a25b0be813cfa846892a5047d8082c7fb5
hTaw99 <employee23.ticlick@gmail.com>
2025-07-16 12:24:53 +0300

    fix: comment out unused navigation links in navlinks.ts

--- constants/navlinks.ts
+++ constants/navlinks.ts
@@ -13,30 +13,30 @@ export const Navlinks = [
     },
     path: Routes.about,
   },
-  {
-    label(label: string = "Gallery") {
-      return label;
-    },
-    path: Routes.gallery,
-  },
-  {
-    label(label: string = "Blogs") {
-      return label;
-    },
-    path: Routes.blogs,
-  },
-  {
-    label(label: string = "Programs") {
-      return label;
-    },
-    path: Routes.programs,
-  },
-  {
-    label(label: string = "Careers") {
-      return label;
-    },
-    path: Routes.careers,
-  },
+  // {
+  //   label(label: string = "Gallery") {
+  //     return label;
+  //   },
+  //   path: Routes.gallery,
+  // },
+  // {
+  //   label(label: string = "Blogs") {
+  //     return label;
+  //   },
+  //   path: Routes.blogs,
+  // },
+  // {
+  //   label(label: string = "Programs") {
+  //     return label;
+  //   },
+  //   path: Routes.programs,
+  // },
+  // {
+  //   label(label: string = "Careers") {
+  //     return label;
+  //   },
+  //   path: Routes.careers,
+  // },
   {
     label(label: string = "Contact") {
       return label;
