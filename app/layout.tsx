d80aff98e6b6d1eb0e28b214357b1e6b68ca2528
hTaw99 <employee23.ticlick@gmail.com>
2025-07-16 13:58:45 +0300

    fix: remove unnecessary Head component wrapping for Google Tag Manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,17 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <Script id="gtm-head" strategy="beforeInteractive">
-          {`
+      <Script id="gtm-head" strategy="beforeInteractive">
+        {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NXW7WLQX');
           `}
-        </Script>
-      </Head>
+      </Script>
       <body
         className={`${Gelion.className} bg-foreground antialiased relative`}
       >
