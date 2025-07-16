e27c228b8e2a1315a2a33ab36ced48929a0ecf0e
hTaw99 <employee23.ticlick@gmail.com>
2025-07-16 19:48:15 +0300

    feat: integrate Google Tag Manager component imported from next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 import { Gelion } from "@/design-system/font";
 import { HEADER_HEIGHT } from "@/constants/header-height";
@@ -22,27 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script id="gtm-head" strategy="beforeInteractive">
-        {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NXW7WLQX');
-          `}
-      </Script>
       <body
         className={`${Gelion.className} bg-foreground antialiased relative`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NXW7WLQX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         <Header />
         <div style={{ minHeight: `calc(100vh - ${HEADER_HEIGHT}px)` }}>
           {children}
@@ -50,6 +31,7 @@ export default function RootLayout({
         <Footer />
 
         <GoogleAnalytics gaId={"G-L73SE4X3QR"} />
+        <GoogleTagManager gtmId={"GTM-NXW7WLQX"} />
       </body>
     </html>
   );
