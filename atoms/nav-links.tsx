4871e4e35f91e2ae338d89f13b94901e3965f126
hTaw99 <employee23.ticlick@gmail.com>
2025-07-16 13:20:38 +0300

    feat: wrap navigation links with Google Events tracking for improved analytics

--- atoms/nav-links.tsx
+++ atoms/nav-links.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 
 import { Navlinks } from "@/constants/navlinks";
+import { GoogleEventsWrapper } from "@/wrappers/google-events-wrapper";
 
 export function NavLinks() {
   return Navlinks.map((link) => (
-    <Link
+    <GoogleEventsWrapper
       key={link.label()}
-      className="uppercase text-white hover:text-lev-yellow whitespace-nowrap"
-      href={link.path}
+      action={`${link.label()} - CLicked`}
+      category="NavBar"
+      label={link.label()}
     >
-      {link.label()}
-    </Link>
+      <Link
+        className="uppercase text-white hover:text-lev-yellow whitespace-nowrap"
+        href={link.path}
+      >
+        {link.label()}
+      </Link>
+    </GoogleEventsWrapper>
   ));
 }
