4f39984b3a68976093e0ff961f13781ca072f39e
hTaw99 <hady.tawfik1999@gmail.com>
2025-09-15 13:30:08 +0300

    feat: update layout metadata with enhanced SEO details and add Twitter card image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,45 @@ import { HEADER_HEIGHT } from "@/constants/header-height";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 
+import twitterCardPng from "@/assets/photos/twitter-card.png"
+
 import type { Metadata } from "next";
 
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Levntura",
+  title:
+    "Study Abroad Agency In Egypt & Jordan | Work And Travel & J1 Visa | Levntura",
+  description:
+    "Levntura is a leading study abroad agency in Egypt & Jordan offering Work and Travel and J1 visa programs, empowering youth with cultural exchange worldwide.",
+  openGraph: {
+    type: "website",
+    url: "https://www.levntura.com/",
+    title:
+      "Study Abroad Agency In Egypt & Jordan | Work And Travel & J1 Visa | Levntura",
+    description:
+      "Levntura is a leading study abroad agency in Egypt & Jordan offering Work and Travel and J1 visa programs, empowering youth with cultural exchange worldwide.",
+    images: [
+      {
+        url: twitterCardPng.src,
+        width: 1200,
+        height: 630,
+        alt: "Levntura",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://www.levntura.com/",
+    title:
+      "Study Abroad Agency In Egypt & Jordan | Work And Travel & J1 Visa | Levntura",
+    description:
+      "Levntura is a leading study abroad agency in Egypt & Jordan offering Work and Travel and J1 visa programs, empowering youth with cultural exchange worldwide.",
+    images: [twitterCardPng.src],
+  },
   other: {
     "facebook-domain-verification": "aplbfnewceubq9qxhed3c4ixeuixxk",
+    "google-site-verification":"sdRE7OEPfgFTN8CzM2zrg2lCDxERdYrKd04ixYEYQ5k"
   },
 };
 
